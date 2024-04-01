16eb5062edf84657d93b24dd56e3115886a64eb5
claudiaaziz <claudiahannahh@gmail.com>
2024-03-31 22:50:35 -0400

    Create add-password end point

--- server/index.js
+++ server/index.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const app = express();
-const mysql = require('mysql')
+const mysql = require("mysql");
 const PORT = 3001;
 
 const db = mysql.createConnection({
-  user: 'root',
-  host: 'localhost',
-  password: '',
-  database: 'PassworManager'
-})
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "PassworManager",
+});
 
-app.get("/", (req, res) => {
-  res.send("♡");
+app.post("/add-password", (req, res) => {
+  const { password, website } = req.body;
+
+  db.query(
+    "INSERT INTO passwords (password, title) VALUES (?,?)",
+    [password, website],
+    (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Success");
+      }
+    }
+  );
 });
 
 app.listen(PORT, () => console.log(`Server is running on, ${PORT}`));
