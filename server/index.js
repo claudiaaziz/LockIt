55b92c81d2b4ecb27b1dc9eb95aa23d286b8e62c
claudiaaziz <claudiahannahh@gmail.com>
2024-03-31 22:25:44 -0400

    Establish connection to db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
-const app = express()
-const PORT = 3001
+const app = express();
+const mysql = require('mysql')
+const PORT = 3001;
 
-app.listen(PORT => console.log(`Server is running on, ${PORT}`))
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: '',
+  database: 'PassworManager'
+})
+
+app.get("/", (req, res) => {
+  res.send("♡");
+});
+
+app.listen(PORT, () => console.log(`Server is running on, ${PORT}`));
