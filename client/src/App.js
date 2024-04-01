c7446bfb860dbc327311a6ace8e7be76356f3e32
claudiaaziz <claudiahannahh@gmail.com>
2024-03-31 22:50:08 -0400

    Create add-password end point

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import { useState } from 'react'
 
 function App() {
+  const [password, setPassword] = useState('')
+  const [website, setWebsite] = useState('')
+
   return (
     <div className="App">
       <div className="add-password-div">
-        <input placeholder="Ex: password123" />
-        <input placeholder="Ex. Github" />
+        <input
+          type="password"
+          placeholder="Ex: password123"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          placeholder="Ex. Github"
+          onChange={(e) => setWebsite(e.target.value)}
+        />
         <button>Add Password</button>
       </div>
     </div>
